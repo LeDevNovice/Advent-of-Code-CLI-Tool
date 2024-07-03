0bc6aaa5caa54ba0c267cfc106304819e6cdff73
GregorySaison <gsaison@apizr.fr>
2024-07-03 13:40:25 +0200

    feat: add logic of package.json retrievment in utils function and add tests for this function

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
-import * as path from 'path';
 import { program } from 'commander';
-import { fileURLToPath } from 'url';
 
 import { registerCommands } from '@commands/index';
+import { getPackageJson } from './utils/getPackageJson';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const packageJsonPath = path.resolve(__dirname, './package.json');
-
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageJson = getPackageJson(import.meta.url);
 
 program.version(packageJson.version);
 registerCommands(program);
